8e477351c1f86b914bb9c7e89281015df6714f94
Nayana395 <nayanajijin@gmail.com>
2024-12-20 11:23:41 +0530

    update app.py and created templats/hello.html

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
-from app import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-   return render_template('Hello.html')
+@app.route("/")
+def my_form():
+   return render_template("hello.html")
 
-if __name__ == '__main__':
-   app.run()
+
+@app.route("/", methods=["POST"])
+def my_form_post():
+   text=request.form['text']
+   return "<h1> {} </h1>".format(text)
+
+
+if __name__=="__main__":
+   app.run(debug=True)
